74a47b6fba9618fb630ddd2cdbdaaaa01dce0bca
Andrew <andrewunifei@gmail.com>
2023-05-23 21:33:35 -0300

    Comunicação com AP

--- front-end/scanner-app/src/App.tsx
+++ front-end/scanner-app/src/App.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HorizontalMenu from './components/HorizontalMenu';
 import MainGrid from './components/MainGrid';
+import { socket } from './socket';
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value: any) {
+      setFooEvents((previous: any): any => [...previous, value]);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+    }
+  })
+
   return (
     <div className="App">
       <HorizontalMenu />
-      <MainGrid />
+      <MainGrid  />
     </div>
   );
 }
