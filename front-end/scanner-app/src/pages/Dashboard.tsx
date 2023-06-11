f856db9c29c38adbf078b81ab192d74a619e725d
Andrew <andrewunifei@gmail.com>
2023-06-11 19:48:21 -0300

    Comunicação com API

--- front-end/scanner-app/src/pages/Dashboard.tsx
+++ front-end/scanner-app/src/pages/Dashboard.tsx
@@ -13,21 +13,28 @@ const { Header, Content, Footer, Sider } = Layout;
 const Dashboard: React.FC = () => {
 
   return (
-      <Layout style={{height: '95vh', border: '1px solid black'}}>
+      <Layout style={{
+        height: '95vh', 
+        border: '1px solid black',
+      }}>
         <Content>
-          <Row style={{
-            height: '15vh',
-            margin: '20px',
+          <Row 
+
+          gutter={16}
+          
+          style={{
+            height: '10vh',
+            margin: '20px 0 ',
           }}>
-            <Col span={4} 
+            <Col className="gutter-row" span={4} 
               style={{
                 background: '#fff',
                 borderRadius: '10px', 
                 boxShadow: '15px 15px 15px rgb(60, 60, 60, 0.1)',
             }}>
-              <p>PnL: 50%</p>
+              <p>Win rate: 50%</p>
             </Col>
-            <Col span={19} offset={1}
+            <Col className="gutter-row" span={20}
               style={{
               background: '#fff',
               borderRadius: '10px', 
@@ -35,6 +42,17 @@ const Dashboard: React.FC = () => {
             }}>
             </Col>
           </Row>
+          {/* <Row style={{
+            height: '60vh',
+            margin: '20px',
+          }}>
+            <Col span={24} style={{
+                background: '#fff',
+                borderRadius: '10px', 
+                boxShadow: '15px 15px 15px rgb(60, 60, 60, 0.1)',
+            }}>
+            </Col>
+          </Row> */}
         </Content>
         <Footer style={{ textAlign: 'center' }}>
           <Divider plain>Fractal Hub • 2023</ Divider>
